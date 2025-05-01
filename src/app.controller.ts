bed159c555c1028f38d8aca6eb8218169dc65048
Marina Khadieva <92573868+MariKhad@users.noreply.github.com>
2025-05-02 01:25:03 +0300

    несколько исправлений

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,9 +8,17 @@ import { BluefinService } from './bluefin/bluefin.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiOperation({ summary: 'Hello message' })
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @ApiOperation({ summary: 'Show all pools on stables' })
+  @Get('/pools')
+  async getAllPools(): Promise<any> {
+    const pools = await this.appService.getAllPools();
+    return pools;
   }
 
   @ApiOperation({ summary: 'Show all pools for a stable coin' })
@@ -24,12 +32,5 @@ export class AppController {
   })
   async getAllPoolsByToken(@Param('token') token: string): Promise<any> {
     return this.appService.getAllTokenPools(token);
-  }
-
-  @ApiOperation({ summary: 'Show all pools on stables' })
-  @Get('/pools')
-  async getAllPools(): Promise<any> {
-    const pools = await this.appService.getAllPools();
-    return pools;
   }
 }
